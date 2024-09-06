0c8effbaa00387166edd26b491fdd309912b9e27
BrenoChristaziano <165391455+BrenoChristaziano@users.noreply.github.com>
2024-09-06 08:34:07 -0300

    números

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const tela = document.querySelector('#caixa')
-const botoes = [...document.querySelectorAll('.numeros')]
+const cdn = document.querySelector('#caixadenumeros')
+let botoes = [...document.querySelectorAll('.numeros')]
 const clear = document.querySelector('#clear')
 const mais = document.querySelector('#mais')
 const vezes = document.querySelector('#vezes')
@@ -8,3 +9,13 @@ const divisao = document.querySelector('#divisao')
 const virgula = document.querySelector('#virgula')
 const igual = document.querySelector('#igual')
 
+
+
+cdn.addEventListener("click", (evt) => {
+    if (evt.target && evt.target.classList.contains('numeros')) {
+        let p = document.createElement('p');
+        p.setAttribute('class', 'nat');
+        p.innerHTML = evt.target.innerHTML;
+        tela.appendChild(p);
+    }
+});
