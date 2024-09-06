09135e42fcdebcc363d4ff7dff70fa3b6299c782
BrenoChristaziano <165391455+BrenoChristaziano@users.noreply.github.com>
2024-09-06 08:45:37 -0300

    mais menos vezes divisão e virgula

--- script.js
+++ script.js
@@ -13,9 +13,51 @@ const igual = document.querySelector('#igual')
 
 cdn.addEventListener("click", (evt) => {
     if (evt.target && evt.target.classList.contains('numeros')) {
-        let p = document.createElement('p');
-        p.setAttribute('class', 'nat');
-        p.innerHTML = evt.target.innerHTML;
-        tela.appendChild(p);
+        let p = document.createElement('p')
+        p.setAttribute('class', 'nat')
+        p.innerHTML = evt.target.innerHTML
+        tela.appendChild(p)
     }
-});
+})
+
+mais.addEventListener("click",(evt)=>{
+    if (mais != mais.checkd){
+        let p = document.createElement('p')
+        p.setAttribute('class', 'nat')
+        p.innerHTML = evt.target.innerHTML
+        tela.appendChild(p)
+    }
+})
+vezes.addEventListener("click",(evt)=>{
+    if (vezes != vezes.checkd){
+        let p = document.createElement('p')
+        p.setAttribute('class', 'nat')
+        p.innerHTML = evt.target.innerHTML
+        tela.appendChild(p)
+    }
+})
+menos.addEventListener("click",(evt)=>{
+    if (menos != menos.checkd){
+        let p = document.createElement('p')
+        p.setAttribute('class', 'nat')
+        p.innerHTML = evt.target.innerHTML
+        tela.appendChild(p)
+    }
+})
+divisao.addEventListener("click",(evt)=>{
+    if (divisao != divisao.checkd){
+        let p = document.createElement('p')
+        p.setAttribute('class', 'nat')
+        p.innerHTML = evt.target.innerHTML
+        tela.appendChild(p)
+    }
+})
+virgula.addEventListener("click",(evt)=>{
+    if (virgula != virgula.checkd){
+        let p = document.createElement('p')
+        p.setAttribute('class', 'nat')
+        p.innerHTML = evt.target.innerHTML
+        tela.appendChild(p)
+    }
+})
+
