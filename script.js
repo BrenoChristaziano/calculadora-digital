8aa0d552de69928bb166ec5eea08909bc103427c
BrenoChristaziano <165391455+BrenoChristaziano@users.noreply.github.com>
2024-09-09 10:10:46 -0300

    meado falho

--- script.js
+++ script.js
@@ -9,55 +9,60 @@ const divisao = document.querySelector('#divisao')
 const virgula = document.querySelector('#virgula')
 const igual = document.querySelector('#igual')
 
+const op = [
+    ()=>{
+        let val1 = botoes.value; let soma = mais; let val2 = botoes.value
+        
+    }
+]
 
+// cdn.addEventListener("click", (evt) => {
+//     if (evt.target && evt.target.classList.contains('numeros')) {
+//         let p = document.createElement('p')
+//         p.setAttribute('class', 'nat')
+//         p.innerHTML = evt.target.innerHTML
+//         tela.appendChild(p)
+//     }
+// })
 
-cdn.addEventListener("click", (evt) => {
-    if (evt.target && evt.target.classList.contains('numeros')) {
-        let p = document.createElement('p')
-        p.setAttribute('class', 'nat')
-        p.innerHTML = evt.target.innerHTML
-        tela.appendChild(p)
-    }
-})
-
-mais.addEventListener("click",(evt)=>{
-    if (mais != mais.checkd){
-        let p = document.createElement('p')
-        p.setAttribute('class', 'nat')
-        p.innerHTML = evt.target.innerHTML
-        tela.appendChild(p)
-    }
-})
-vezes.addEventListener("click",(evt)=>{
-    if (vezes != vezes.checkd){
-        let p = document.createElement('p')
-        p.setAttribute('class', 'nat')
-        p.innerHTML = evt.target.innerHTML
-        tela.appendChild(p)
-    }
-})
-menos.addEventListener("click",(evt)=>{
-    if (menos != menos.checkd){
-        let p = document.createElement('p')
-        p.setAttribute('class', 'nat')
-        p.innerHTML = evt.target.innerHTML
-        tela.appendChild(p)
-    }
-})
-divisao.addEventListener("click",(evt)=>{
-    if (divisao != divisao.checkd){
-        let p = document.createElement('p')
-        p.setAttribute('class', 'nat')
-        p.innerHTML = evt.target.innerHTML
-        tela.appendChild(p)
-    }
-})
-virgula.addEventListener("click",(evt)=>{
-    if (virgula != virgula.checkd){
-        let p = document.createElement('p')
-        p.setAttribute('class', 'nat')
-        p.innerHTML = evt.target.innerHTML
-        tela.appendChild(p)
-    }
-})
+// mais.addEventListener("click",(evt)=>{
+//     if (mais != mais.checkd){
+//         let p = document.createElement('p')
+//         p.setAttribute('class', 'nat')
+//         p.innerHTML = evt.target.innerHTML
+//         tela.appendChild(p)
+//     }
+// })
+// vezes.addEventListener("click",(evt)=>{
+//     if (vezes != vezes.checkd){
+//         let p = document.createElement('p')
+//         p.setAttribute('class', 'nat')
+//         p.innerHTML = evt.target.innerHTML
+//         tela.appendChild(p)
+//     }
+// })
+// menos.addEventListener("click",(evt)=>{
+//     if (menos != menos.checkd){
+//         let p = document.createElement('p')
+//         p.setAttribute('class', 'nat')
+//         p.innerHTML = evt.target.innerHTML
+//         tela.appendChild(p)
+//     }
+// })
+// divisao.addEventListener("click",(evt)=>{
+//     if (divisao != divisao.checkd){
+//         let p = document.createElement('p')
+//         p.setAttribute('class', 'nat')
+//         p.innerHTML = evt.target.innerHTML
+//         tela.appendChild(p)
+//     }
+// })
+// virgula.addEventListener("click",(evt)=>{
+//     if (virgula != virgula.checkd){
+//         let p = document.createElement('p')
+//         p.setAttribute('class', 'nat')
+//         p.innerHTML = evt.target.innerHTML
+//         tela.appendChild(p)
+//     }
+// })
 
