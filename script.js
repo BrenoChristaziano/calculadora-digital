aeb40b28f2b2816fa917fe8d38c6aa51072717a3
BrenoChristaziano <165391455+BrenoChristaziano@users.noreply.github.com>
2024-09-17 09:44:08 -0300

    recomeço do novo codigo

--- script.js
+++ script.js
@@ -9,14 +9,33 @@ let divisao = document.querySelector('#divisao')
 let virgula = document.querySelector('#virgula')
 let igual = document.querySelector('#igual')
 
-let novoElemento = (tipo) => {
-
+let novoElemento = (conteudo) => {
+    const p = document.createElement('p')
+    p.setAttribute('class', 'nat')
+    p.textContent = conteudo
+    return p
 }
 
 botoes.forEach((botao) => {
-    botoes.forEach((botao) => {
-        botao.addEventListener('click', () => {
-            const numero = botao.textContent           
-        })
+    botao.addEventListener("click", () => {
+        const numero = botao.textContent
+        let elemento  = novoElemento(numero) 
+        tela.appendChild(elemento)          
     })
+})
+
+mais.addEventListener("click", (evt)=>{
+        if (mais != mais.checked) {
+            let adiçao = mais.textContent
+            let novo = novoElemento(adiçao)
+            tela.appendChild(novo)
+        }
+})
+
+igual.addEventListener("click", (evt) =>{
+    const num = Number(botoes)
+    if (num == !isNaN(num)){
+        let soma = num = num
+        tela.appendChild(soma)
+    }
 })
