e77f6700c0ad73bc1f0294e61dd49516ab3ed3b1
BrenoChristaziano <165391455+BrenoChristaziano@users.noreply.github.com>
2024-09-17 08:31:06 -0300

    erro

--- script.js
+++ script.js
@@ -9,35 +9,14 @@ let divisao = document.querySelector('#divisao')
 let virgula = document.querySelector('#virgula')
 let igual = document.querySelector('#igual')
 
-let novoElemento = (tipo, conteudo) =>{
-    let elemento 
-    switch (tipo) {
-        case 'p':
-        let p = document.createElement('p')
-        p.setAttribute('class', 'nat')
-        break
-        case 'd1':
-        let d1 = document.createElement('div')
-        p.setAttribute('class', 'div1')
-        break
-        case 'd2':
-        let d2 = document.createElement('d2')
-        p.setAttribute('class', 'div2')
-        break
-    }
-    if (conteudo){
-        elemento.textContent = conteudo
-    }
-    tela.appendChild(elemento)   
+let novoElemento = (tipo) => {
+
 }
+
 botoes.forEach((botao) => {
     botoes.forEach((botao) => {
         botao.addEventListener('click', () => {
-            // Obtém o número do texto do botão clicado
-            const numero = botao.textContent
-            // Chama a função novoElemento com o número clicado
-            novoElemento('d1', numero)
-            tela.appendChild(numero)
+            const numero = botao.textContent           
         })
     })
 })
