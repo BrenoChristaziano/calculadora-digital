d956f5cb75a24d5c1ee836239b1a248a85431c37
BrenoChristaziano <165391455+BrenoChristaziano@users.noreply.github.com>
2024-09-19 10:32:35 -0300

    recriando novamente

--- script.js
+++ script.js
@@ -1,57 +1,17 @@
-const tela = document.querySelector('#caixa')
-const cdn = document.querySelector('#caixadenumeros')
-let botoes = [...document.querySelectorAll('.numeros')]
-const clear = document.querySelector('#clear')
-let mais = document.querySelector('#mais')
-let vezes = document.querySelector('#vezes')
-let menos = document.querySelector('#menos')
-let divisao = document.querySelector('#divisao')
-let virgula = document.querySelector('#virgula')
-let igual = document.querySelector('#igual')
-
-
-let primeiroNumero = null
-let operacao = null
-
-let novoElemento = (conteudo) => {
-    const p = document.createElement('p')
-    p.setAttribute('class', 'nat')
-    p.textContent = conteudo
-    return p
+function adicionar(valor) {
+    document.getElementById("tela").value += valor
+    let caixa = document.getElementById("caixa")
+    caixa.appendChild(Number(valor))
+       
+}
+function calcular() {
+    const resultado =document.getElementById("caixa").value
+    document.getElementById("caixa").value = resultado
+}
+function limpar() {
+    document.getElementById("caixa").value = ""
 }
 
-botoes.forEach((botao) => {
-    botao.addEventListener("click", () => {
-        const numero = botao.textContent
-        tela.appendChild(novoElemento(numero))
 
-        // Se não houver uma operação, armazenar o primeiro número
-        if (operacao === null) {
-            primeiroNumero = Number(numero)
-        }
-    })
-})
-
-mais.addEventListener("click", () => {
-    if (primeiroNumero !== null) {
-        operacao = "soma" // Definindo a operação como soma
-        tela.appendChild(novoElemento("+")) // Mostrando a operação na tela
-    }
-});
-
-igual.addEventListener("click", () => {
-    if (primeiroNumero !== null && operacao === "soma") {
-        // Pega o segundo número (último número inserido)
-        const numerosNaTela = [...tela.querySelectorAll('.nat')]
-        const segundoNumero = Number(numerosNaTela[numerosNaTela.length - 1].textContent)
-
-        // Realiza a soma
-        const resultado = primeiroNumero + segundoNumero
-        tela.value = ""
-        tela.appendChild(novoElemento(resultado))
-
-        // Reiniciar as variáveis para nova operação
-        primeiroNumero = null
-        operacao = null
-    }
-});
+// para o usocorreto do eval 
+//(/^[0-9+\-*/().\s]*$/.test(expressao)
