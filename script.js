f789556394c683b7add4a17e104ffef217b9a455
BrenoChristaziano <165391455+BrenoChristaziano@users.noreply.github.com>
2024-09-24 09:54:16 -0300

    alterações

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-function adicionar(valor) {
-    document.getElementById("tela").value += valor
-    let caixa = document.getElementById("caixa")
-    caixa.appendChild(Number(valor))
-       
+function adicionar(value) {
+    const tela = document.getElementById('caixa');
+    tela.value += value;
 }
+
 function calcular() {
-    const resultado =document.getElementById("caixa").value
-    document.getElementById("caixa").value = resultado
+    document.getElementById('caixa').value = '';
 }
+
 function limpar() {
-    document.getElementById("caixa").value = ""
+    const display = document.getElementById('');
+    try {
+        // Usando math.js para avaliar a expressão
+        display.value = math.evaluate(display.value);
+    } catch (error) {
+        display.value = 'Erro';
+    }
 }
+// function adicionar(valor) {
+//     document.getElementById("tela").value += valor
+//     let caixa = document.getElementById("caixa")
+//     caixa.appendChild(Number(valor))
+       
+// }
+// function calcular() {
+//     const resultado =document.getElementById("caixa").value
+//     document.getElementById("caixa").value = resultado
+// }
+// function limpar() {
+//     document.getElementById("caixa").value = ""
+// }
+
 
 
 // para o usocorreto do eval 
