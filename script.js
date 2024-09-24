d79f3e34956ad699d17e7a08168c236554634ba9
BrenoChristaziano <165391455+BrenoChristaziano@users.noreply.github.com>
2024-09-24 09:55:30 -0300

    mais aterações

--- script.js
+++ script.js
@@ -1,36 +1,13 @@
-function adicionar(value) {
-    const tela = document.getElementById('caixa');
-    tela.value += value;
-}
-
-function calcular() {
-    document.getElementById('caixa').value = '';
-}
-
-function limpar() {
-    const display = document.getElementById('');
-    try {
-        // Usando math.js para avaliar a expressão
-        display.value = math.evaluate(display.value);
-    } catch (error) {
-        display.value = 'Erro';
-    }
-}
-// function adicionar(valor) {
-//     document.getElementById("tela").value += valor
-//     let caixa = document.getElementById("caixa")
-//     caixa.appendChild(Number(valor))
+function adicionar(valor) {
+     document.getElementById("tela").value += valor
+     let caixa = document.getElementById("caixa")
+    caixa.appendChild(Number(valor))
        
-// }
-// function calcular() {
-//     const resultado =document.getElementById("caixa").value
-//     document.getElementById("caixa").value = resultado
-// }
-// function limpar() {
-//     document.getElementById("caixa").value = ""
-// }
-
-
-
-// para o usocorreto do eval 
-//(/^[0-9+\-*/().\s]*$/.test(expressao)
+}
+function calcular() {
+    const resultado =document.getElementById("caixa").value
+    document.getElementById("caixa").value = resultado
+}
+function limpar() {
+    document.getElementById("caixa").value = ""
+}
